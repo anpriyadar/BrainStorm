f69c5528e6b32e6fb99bef79e2f29ca63fdd600e
anpriyadar <animesh11.priyadarshi@nttdata.com>
2017-02-27 01:36:16 +0530

    search Bar implementation and reloading--In Progress

--- brainStorm/src/js/components/countries/index.js
+++ brainStorm/src/js/components/countries/index.js
@@ -3,14 +3,20 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
   View,
   TouchableOpacity,
-  Alert
+  Alert,
+  ScrollView
 } from 'react-native';
 
 import Listview from '../shared/listview/listview.js'
 import ListviewRow from '../shared/listview/listviewrow.js'
 import mappers from '../../mappers/countriesmapper.js'
+import componentStyles from '../../../styles/base.js'
+import RNRestart from 'react-native-restart';
+
+const s = StyleSheet.create(componentStyles)
 
 
 export default class Countries extends Component {
@@ -18,8 +24,9 @@ export default class Countries extends Component {
 
   super(props)
   this.state = {
-      data: []
-    }
+      data: [],
+      reload:false
+            }
     const { navigator } = this.props
 
     navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
@@ -27,18 +34,34 @@ export default class Countries extends Component {
 
 
 }
+componentWillMount(){
+mappers.getCountryDetails().then((res)=>{
+  this.setState({
+ data: res
+ })
+})
+}
+
 static navigatorButtons = {
   rightButtons: [
     {
-      title: 'Edit',
-      id: 'edit'
+      title: 'Reload',
+      id: 'reload'
     },
-    {
-     title: 'Add',
-      id: 'add'
-    }
   ]
 };
+
+onNavigatorEvent(event) {
+  const rawdata=this.state.data
+  const reload=this.state.reload
+  const dataSource = rawdata.map((item) =>item)
+  if (event.id === 'reload' && reload===true) {
+  RNRestart.Restart()
+  }
+  else{
+    alert('Reload can be only performed after search')
+  }
+}
 
 handleRowClick(data) {
   const { navigator } = this.props
@@ -51,20 +74,17 @@ handleRowClick(data) {
   });
   }
 
-
-onNavigatorEvent(event) {
-  if (event.id === 'edit') {
-    Alert.alert('NavBar', 'Edit button pressed');
-  }
-  if (event.id === 'add') {
-    Alert.alert('NavBar', 'Add button pressed');
-  }
-}
-componentWillMount(){
-mappers.getCountryDetails().then((res)=>{
-  this.setState({
- data: res
- })
+filterSearch(text){
+  const rawdata = this.state.data
+  const dataSource = rawdata.map((item) =>item)
+  const newData = dataSource.filter((item)=>{
+  const itemData = item.name.toUpperCase()
+  const textData = text.toUpperCase()
+  return itemData.indexOf(textData) > -1
+})
+this.setState({
+data: newData,
+reload:true
 })
 }
 
@@ -73,17 +93,27 @@ showListView()
   const { navigator } = this.props
  const rawdata=this.state.data
  const dataSource = rawdata.map((item) =>item)
+ const SearchText= this.state.text
+ const placeHolderText = 'Search by Country Name'
 
   return(
+    <View style={s.flexFull}>
+    <TextInput
+      style={s.searchBar}
+      placeholder={placeHolderText}
+      onChangeText={(text)=>this.filterSearch(text)}
+      />
     <Listview
         data = {dataSource}
         navigator={navigator}
         handleRowClick={this.handleRowClick}
         listviewRow = {ListviewRow}/>
+      </View>
   )
 }
 
   render() {
+    console.log(this.state.data)
     return (
       this.showListView()
     )
